42564ca60c7fd105fc4d27944bb831ef5498f8ed
riesdepies <riesdepies@gmail.com>
2025-10-02 18:04:44 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,8 +7,6 @@ module.exports = (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS');
-
-    // Cache de manifest en streamlijst (met de resolver URL) voor 1 uur.
     res.setHeader('Cache-Control', 'public, s-maxage=3600, stale-while-revalidate=1800');
 
     router(req, res, () => {
